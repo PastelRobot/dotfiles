59e66aa45bff4011b2f433734d5e4e311b63fb8c
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-10-07 14:59:50 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 #This script requires you do do a few things before running.
-#1:Install base-devel
-  #(sudo pacman -S base-devel)
-#2:Install yay 
-  #(git clone https://aur.archlinux.org/yay-git.git
-  #cd yay-git
-  #makepkg -si
-  #cd)
-#3: enable multilib
+#1: enable multilib
   #(sudo nano /etc/pacman.conf
   #and uncomment the multilib section)
-#4:and make exacutable(obvs)
+#2:and make exacutable(obvs)
   #(sudo chmod +x installscript.sh)
   
   
 #Obligitory update
 sudo pacman -Syu
 #Essentials
-sudo pacman -S xf86-video-amdgpu xorg xorg-xinit xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
+sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
 #bulk install
 sudo pacman -S i3 python python-pip polkit xfce4-power-manager wine-staging clipit avahi nm-connection-editor pavucontrol vlc lxappearance ncurses steam libreoffice-fresh linux-headers lutris dunst discord dolphin ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker reflector
 #Install moniter swap
