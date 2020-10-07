9a91bdcf26d869fd6d0330cca10133fce8cef578
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-10-07 13:41:26 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -26,9 +26,14 @@ git clone https://github.com/einzigartigerName/i3-workspace-swap.git
 cd i3-workspace-swap
 python setup.py install
 cd
+#install yay
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg-si
+cd
 #less essential essentials.
 sudo pacman -S polkit xfce4-power-manager wine-staging clipit avahi nm-connection-editor pavucontrol vlc lxappearance ncurses steam libreoffice-fresh linux-headers lutris dunst discord dolphin ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker reflector
-sudo -u riley yay -S argon steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git
+yay -S argon steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git
 #enable services
 sudo systemctl enable lightdm.boot
 sudo systemctl enable reflector
