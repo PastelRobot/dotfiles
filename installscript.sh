7fc7190a98cc5f28210c10f63c8e8982c56484c5
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-10-08 04:06:56 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -13,11 +13,11 @@ sudo pacman -Syu
 #Essentials
 sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
 #bulk install
-sudo pacman -S i3 python python-pip polkit
+sudo pacman -S i3 python python-pip polkit i3status-rust dmenu awesome-terminal-fonts powerline-fonts
 sudo pacman -S xfce4-power-manager wine-staging clipit avahi nm-connection-editor pavucontrol vlc
 sudo pacman -S libreoffice-fresh linux-headers lutris dunst discord dolphin ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
 sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker reflector
-sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons dolphin-plugins
+sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons dolphin-plugins python-i3ipc
 #Install moniter swap
 git clone https://github.com/einzigartigerName/i3-workspace-swap.git
 cd i3-workspace-s
