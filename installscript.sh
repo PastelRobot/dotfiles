1490b0d10e33ed95d7c067feca22a9cf78148290
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-10-08 03:17:00 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -14,9 +14,10 @@ sudo pacman -Syu
 sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
 #bulk install
 sudo pacman -S i3 python python-pip polkit
-sudo pacman -S xfce4-power-manager wine-staging clipit avahi nm-connection-editor pavucontrol vlc lxappearance ncurses steam
+sudo pacman -S xfce4-power-manager wine-staging clipit avahi nm-connection-editor pavucontrol vlc
 sudo pacman -S libreoffice-fresh linux-headers lutris dunst discord dolphin ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
 sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker reflector
+sudo pacman -S lxappearance ncurses steam
 #Install moniter swap
 git clone https://github.com/einzigartigerName/i3-workspace-swap.git
 cd i3-workspace-s
