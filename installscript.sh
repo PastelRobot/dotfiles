7a49fa322cc71c9821071f1738daf54aee019ed9
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-10-14 04:45:03 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -14,18 +14,25 @@ sudo pacman -Syu
 sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
 #bulk install
 sudo pacman -S i3 python python-pip polkit i3status-rust dmenu awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts
-sudo pacman -S xfce4-power-manager wine-staging clipit avahi nm-connection-editor pavucontrol vlc
+sudo pacman -S xfce4-power-manager wine-staging clipit avahi nm-connection-editor pavucontrol
 sudo pacman -S libreoffice-fresh linux-headers lutris dunst discord pcmanfm-gtk3 ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
 sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker reflector
-sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons python-i3ipc notepadqq
+sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons python-i3ipc notepadqq vlc
+#install snapd
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+cd
 #install yay
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si
 cd
 #less essential essentials.
-yay -S octopi steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct
+yay -S octopi steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct i3exit
 sudo pip install i3-workspace-swap
+sudo snap install spotify
 #enable services
 sudo systemctl enable lightdm
 sudo systemctl enable reflector
+sudo systemctl enable snapd.socket
