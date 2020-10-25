9c6a6074819da4d6c7e560b23df33e1d395032ea
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-10-25 07:32:39 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -14,7 +14,7 @@ sudo pacman -Syu
 sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
 #bulk install
 sudo pacman -S i3 python python-pip polkit i3status-rust dmenu awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts
-sudo pacman -S xfce4-power-manager wine-staging clipit avahi nm-connection-editor pavucontrol
+sudo pacman -S xfce4-power-manager wine-staging avahi nm-connection-editor pavucontrol
 sudo pacman -S notepadqq linux-headers lutris dunst discord nemo ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
 sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker reflector
 sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons python-i3ipc libreoffice-fresh vlc
@@ -30,7 +30,7 @@ cd yay-git
 makepkg -si
 cd
 #less essential essentials.
-yay -S octopi steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct i3exit
+yay -S octopi steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct i3exit clipit
 sudo pip install i3-workspace-swap
 #enable services
 sudo systemctl enable lightdm
