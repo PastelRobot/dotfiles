874dea0286109220588f93a63357c460df990a49
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-10-28 03:59:44 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -11,14 +11,14 @@
 #Obligitory update
 sudo pacman -Syu
 #Essentials
-sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
+sudo pacman -S xf86-video-amdgpu base-devel wayland xorg-server-xwayland xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
 #bulk install
-sudo pacman -S i3 python python-pip polkit i3status-rust dmenu awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts
+sudo pacman -S sway python python-pip polkit waybar bemenu awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts
 sudo pacman -S xfce4-power-manager wine-staging avahi nm-connection-editor pavucontrol
 sudo pacman -S notepadqq linux-headers lutris dunst discord nemo ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
 sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker reflector
 sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons python-i3ipc libreoffice-fresh vlc
-sudo pacman -S nemo-fileroller nemo-image-converter nemo-preview libu2f-host
+sudo pacman -S nemo-fileroller nemo-image-converter nemo-preview libu2f-host grim
 #install snapd
 git clone https://aur.archlinux.org/snapd.git
 cd snapd
@@ -30,7 +30,7 @@ cd yay-git
 makepkg -si
 cd
 #less essential essentials.
-yay -S octopi steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct i3exit clipit
+yay -S octopi steam-fonts wqy-zenhei splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct i3exit clipit
 sudo pip install i3-workspace-swap
 #enable services
 sudo systemctl enable lightdm
