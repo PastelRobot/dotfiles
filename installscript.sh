02122a307d282dab1c8eaf2cf3ffe4e60d31567c
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-10-29 06:11:43 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -16,7 +16,7 @@ sudo pacman -S xf86-video-amdgpu base-devel wayland xorg-server-xwayland xdg-uti
 sudo pacman -S sway python python-pip polkit waybar bemenu awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts ttf-dejavu ttf-linux-libertine
 sudo pacman -S xfce4-power-manager wine-staging avahi nm-connection-editor pavucontrol bdf-unifont ttf-hanazono
 sudo pacman -S notepadqq linux-headers lutris dunst discord nemo ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
-sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker reflector
+sudo pacman -S pavucontrol gdm reflector
 sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons python-i3ipc libreoffice-fresh vlc
 sudo pacman -S nemo-fileroller nemo-image-converter nemo-preview libu2f-host grim slurp
 #install snapd
@@ -33,6 +33,6 @@ cd
 yay -S octopi steam-fonts wqy-zenhei splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct i3exit clipit
 sudo pip install i3-workspace-swap
 #enable services
-sudo systemctl enable lightdm
+sudo systemctl enable gdm
 sudo systemctl enable reflector
 sudo systemctl enable snapd.socket
