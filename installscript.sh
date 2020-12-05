90f44aed62dea8d2f11fe6c2e2d5f0e08c3ff0db
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-12-05 05:49:58 -0800

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -16,14 +16,9 @@ sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen p
 sudo pacman -S i3 python python-pip polkit i3status-rust dmenu awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts
 sudo pacman -S xfce4-power-manager wine-staging avahi nm-connection-editor network-manager-applet ffmpegthumbnailer libgsf libopenraw tumbler ristretto
 sudo pacman -S notepadqq linux-headers lutris dunst discord nemo ttf-font pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
-sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector
+sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector flatpack
 sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons python-i3ipc libreoffice-fresh vlc
 sudo pacman -S nemo-fileroller nemo-image-converter nemo-preview libu2f-host noto-fonts-emoji gnu-free-fonts ttf-arphic-uming ttf-indic-otf
-#install snapd
-git clone https://aur.archlinux.org/snapd.git
-cd snapd
-makepkg -si
-cd
 #install yay
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
@@ -35,4 +30,3 @@ sudo pip install i3-workspace-swap
 #enable services
 sudo systemctl enable lightdm
 sudo systemctl enable reflector
-sudo systemctl enable snapd.socket
