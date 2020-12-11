da2b2f7e15db252b3ce184910a932f9178817ac4
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2020-12-11 02:03:32 -0800

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -11,11 +11,11 @@
 #Obligitory update
 sudo pacman -Syu
 #Essentials
-sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen picom alacritty nano firefox mesa lib32-mesa
+sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen picom alacritty tilda nano firefox mesa lib32-mesa linux-zen linux-zen-headers
 #bulk install
 sudo pacman -S i3 python python-pip polkit i3status-rust dmenu awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts
 sudo pacman -S xfce4-power-manager wine-staging avahi nm-connection-editor network-manager-applet ffmpegthumbnailer libgsf libopenraw tumbler ristretto
-sudo pacman -S notepadqq linux-headers lutris dunst nemo ttf-font pipewire pipewire-docs pipewire-alsa pipewire-pulse pipewire-jack
+sudo pacman -S notepadqq lutris dunst nemo ttf-font pipewire pipewire-alsa pipewire-pulse pipewire-jack
 sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector flatpack
 sudo pacman -S lxappearance ncurses steam breeze-gtk breeze-icons python-i3ipc libreoffice-fresh vlc
 sudo pacman -S nemo-fileroller nemo-image-converter nemo-preview libu2f-host noto-fonts-emoji gnu-free-fonts ttf-arphic-uming ttf-indic-otf
@@ -25,7 +25,7 @@ cd yay-git
 makepkg -si
 cd
 #less essential essentials.
-yay -S octopi steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct i3exit clipit
+yay -S octopi steam-fonts wqy-zenhei i3-scrot splatmoji-git easystroke-git xfce-polkit-git pa-applet-git qt5ct i3exit network-manager-applet clipit pa-applet-get breeze-obsidian-cursor-theme
 sudo pip install i3-workspace-swap
 #enable services
 sudo systemctl enable lightdm
