a4bfdb48822a7a60cfad564ce627b51efdf62fa5
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2021-01-12 08:10:23 -0800

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -16,7 +16,7 @@ sudo pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen p
 sudo pacman -S i3 python python-pip polkit i3status-rust rofi awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts
 sudo pacman -S xfce4-power-manager wine-staging avahi nm-connection-editor network-manager-applet ffmpegthumbnailer libgsf libopenraw tumbler ristretto
 sudo pacman -S notepadqq lutris dunst nemo ttf-font pipewire pipewire-alsa pipewire-pulse pipewire-jack
-sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector flatpak
+sudo pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector flatpak dex
 sudo pacman -S lxappearance ncurses lxrandr-gtk3 steam breeze-gtk breeze-icons python-i3ipc libreoffice-fresh vlc
 sudo pacman -S nemo-fileroller nemo-image-converter nemo-preview libu2f-host noto-fonts-emoji gnu-free-fonts ttf-arphic-uming ttf-indic-otf
 #install yay
@@ -26,7 +26,7 @@ makepkg -si
 cd
 #less essential essentials.
 yay -S octopi steam-fonts wqy-zenhei i3-scrot splatmoji-git xfce-polkit-git qt5ct i3exit network-manager-applet clipit breeze-obsidian-cursor-theme
-yay -S autotiling
+yay -S autotiling volctl
 sudo pip install i3-workspace-swap
 #enable services
 sudo systemctl enable lightdm
