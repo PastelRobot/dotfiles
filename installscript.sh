b5e4b298d935172b5293d760d37e3db5564bf53e
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2021-09-02 14:10:44 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -16,7 +16,7 @@ doas pacman -S xf86-video-amdgpu base-devel xorg xorg-xinit xdg-utils nitrogen p
 doas pacman -S i3 python python-pip polkit i3status-rust rofi awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts starship
 doas pacman -S xfce4-power-manager wine-staging avahi nm-connection-editor network-manager-applet ffmpegthumbnailer libgsf libopenraw tumbler ristretto
 doas pacman -S notepadqq lutris xfce4-notifyd nemo ttf-font pipewire pipewire-alsa pipewire-pulse pipewire-jack
-doas pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector flatpak maim peek
+doas pacman -S pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector maim peek
 doas pacman -S lxappearance ncurses lxrandr-gtk3 steam breeze-gtk breeze-icons python-i3ipc libreoffice-fresh mpv unarchiver redshift
 doas pacman -S nemo-fileroller nemo-image-converter nemo-preview noto-fonts-emoji gnu-free-fonts ttf-arphic-uming ttf-indic-otf
 doas pacman -S libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
