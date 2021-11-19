19695dd78bd909112ca6e1e2358eadae57644433
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2021-11-19 12:51:58 -0800

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -26,7 +26,7 @@ cd paru
 makepkg -si
 cd ~
 #less essential essentials.
-paru -S steam-fonts wqy-zenhei splatmoji-git xfce-polkit-git qt5ct i3exit network-manager-applet clipit
+paru -S steam-fonts wqy-zenhei splatmoji-git xfce-polkit-git qt5ct network-manager-applet clipit
 paru -S autotiling volctl mailspring noisetorch-bin dracula-gtk-theme-git
 doas pip install i3-workspace-swap
 #Install zap
