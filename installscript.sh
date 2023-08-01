e6cc9f824cbf97e01b6a62ac86b8fd068f816961
PastelRobot <66032933+PastelRobot@users.noreply.github.com>
2023-08-01 00:15:38 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -16,7 +16,7 @@ doas pacman -S xf86-video-amdgpu base-devel wayland xorg-xwayland xdg-utils kitt
 doas pacman -S hyprland xdg-desktop-portal-hyprland python python-pip polkit awesome-terminal-fonts powerline-fonts adobe-source-code-pro-fonts starship
 doas pacman -S polkit-kde-agent wine-staging avahi ffmpegthumbnailer libgsf libopenraw
 doas pacman -S kate hyprpaper lutris ttf-font pipewire pipewire-alsa pipewire-pulse pipewire-jack
-doas pacman -S mako amd-ucode qt5-wayland qt6-wayland wofi copyq thunar
+doas pacman -S mako amd-ucode qt5-wayland qt6-wayland wofi copyq thunar flameshot cpupower
 doas pacman -S eww-wayland ncurses steam python-i3ipc libreoffice-fresh mpv mvi unarchiver
 doas pacman -S tumbler gvfs thunar-archive-plugin thunar-volman noto-fonts-emoji gnu-free-fonts ttf-arphic-uming ttf-indic-otf
 doas pacman -S libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau gvfs-mtp
